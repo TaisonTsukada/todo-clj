debe77da6961fc5942e3478e83b359970756f365
TaisonTsukada <tukadataison1208@gmail.com>
2025-04-07 23:05:18 +0900

    second commit

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -3,14 +3,65 @@
 
 (defonce server (atom nil))
 
+(defn ok [body]
+  {
+   :status 200
+   :body body
+  })
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 Not Found</h1>"})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(def todo-list
+  [
+   {:title "朝ご飯を作る"}
+   {:title "燃えるゴミを出す"}
+   {:title "卵を買って帰る"}
+   {:title "お風呂を洗う"}
+  ])
+
+(defn todo-index-view [req]
+  `("<h1>TODO 一覧</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} todo-list]
+        (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+
+(defn home-view [req]
+  "<h1>ホーム画面</h1>
+   <a href=\"/todo\"> TODO 一覧</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+(defn match-route [uri]
+  (get routes uri))
+
 (defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, World!"})
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
