a3cf34571a7464ba7fdf7044f0df377864c4d9e7
TaisonTsukada <tukadataison1208@gmail.com>
2025-04-13 14:20:23 +0900

    ルーティングとレスポンス処理を改善し、メインハンドラとTODOハンドラを追加しました。また、サーバー起動時の設定を更新し、不要なコードを削除しました。

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,67 +1,19 @@
 (ns todo-clj.core
-  (:require [ring.adapter.jetty :as server]))
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [todo-clj.handler.main :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
 
 (defonce server (atom nil))
 
-(defn ok [body]
-  {
-   :status 200
-   :body body
-  })
-
-(defn not-found []
-  {:status 404
-   :body "<h1>404 Not Found</h1>"})
-
-(defn html [res]
-  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
-
-(def todo-list
-  [
-   {:title "朝ご飯を作る"}
-   {:title "燃えるゴミを出す"}
-   {:title "卵を買って帰る"}
-   {:title "お風呂を洗う"}
-  ])
-
-(defn todo-index-view [req]
-  `("<h1>TODO 一覧</h1>"
-    "<ul>"
-    ~@(for [{:keys [title]} todo-list]
-        (str "<li>" title "</li>"))
-    "</ul>"))
-
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      ok
-      html))
-
-(defn home-view [req]
-  "<h1>ホーム画面</h1>
-   <a href=\"/todo\"> TODO 一覧</a>")
-
-(defn home [req]
-  (-> (home-view req)
-      ok
-      html))
-
-(def routes
-  {"/" home
-   "/todo" todo-index})
-
-(defn match-route [uri]
-  (get routes uri))
-
-(defn handler [req]
-  (let [uri (:uri req)
-        maybe-fn (match-route uri)]
-    (if maybe-fn
-      (maybe-fn req)
-      (not-found))))
+(def app
+  (routes
+   todo-routes
+   main-routes))
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
@@ -69,5 +21,6 @@
     (reset! server nil)))
 
 (defn restart-server []
-  (stop-server)
-  (start-server))
+  (when @server
+    (stop-server)
+    (start-server)))
